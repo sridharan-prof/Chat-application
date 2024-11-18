e3b9318291f95f78c72e16ba9061028a47d458e2
sridharan-prof <sridharan.prof@gmail.com>
2024-11-18 11:21:18 +0530

    chat application

---
+++ chatbot/chatbot/asgi.py
@@ -0,0 +1,32 @@
+"""
+ASGI config for chatbot project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter , URLRouter
+from chat import routing
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
+
+application = ProtocolTypeRouter(
+    {
+        "http" : get_asgi_application() , 
+        "websocket" : AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )    
+        )
+    }
+)
+
+ASGI_APPLICATION = 'chatbot.asgi.application'
+
